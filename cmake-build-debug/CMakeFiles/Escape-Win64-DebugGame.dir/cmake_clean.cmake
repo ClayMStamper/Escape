5cf87bd98feca1c6555ef85f7c94f3bfc3f4e143
Clayton Stamper <claystamper@gmail.com>
2020-08-30 11:14:42 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Escape-Win64-DebugGame.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Escape-Win64-DebugGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Escape-Win64-DebugGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Escape.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Escape"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Escape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
